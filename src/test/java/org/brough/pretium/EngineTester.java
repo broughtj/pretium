08b0c3f3f602aa1ffd969ed0130a4bd7252e0778
Stanford Cline <A01544859@aggies.usu.edu>
2019-03-22 15:04:04 -0600

    added test for the americanBinomialPricer

--- src/test/java/org/brough/pretium/EngineTester.java
+++ src/test/java/org/brough/pretium/EngineTester.java
@@ -2,6 +2,7 @@ package org.brough.pretium;
 
 import org.brough.pretium.Payoff.VanillaCallPayoff;
 import org.brough.pretium.Payoff.VanillaPutPayoff;
+import org.brough.pretium.Pricer.AmericanBinomialPricer;
 import org.brough.pretium.Pricer.EuropeanBinomialPricer;
 import org.brough.pretium.PricingEngine.BinomialPricingEngine;
 import org.brough.pretium.Util.Util;
@@ -60,5 +61,33 @@ public class EngineTester {
 
         OptionFacade facadPut = new OptionFacade(putOption, engine, data);
         assertEquals(facadPut.price(euroBinom), 2.8866818183425997, .00001);
+    }
+
+    @Test
+    public void testAmericanPricer() {
+        double strike = 40.0;
+        double expiry = 1.0;
+        VanillaCallPayoff theCall = new VanillaCallPayoff(strike);
+        VanillaPutPayoff thePut = new VanillaPutPayoff(strike);
+        Option callOption = new Option(expiry, strike, theCall);
+        Option putOption = new Option(expiry, strike, thePut);
+
+        // MarketData
+        double spot = 41.0;
+        double rate = 0.08;
+        double vol = 0.3;
+        double div = 0.0;
+        MarketData data = new MarketData(rate, spot, vol, div);
+
+        // PricingEngine
+        int steps = 1000;
+        AmericanBinomialPricer amerBinom = new AmericanBinomialPricer(steps);
+        BinomialPricingEngine engine = new BinomialPricingEngine(steps, amerBinom);
+
+        OptionFacade facadeCall = new OptionFacade(callOption, engine, data);
+        assertEquals(facadeCall.price(amerBinom), 6.962027962879, .0001);
+
+        OptionFacade facadPut = new OptionFacade(putOption, engine, data);
+        assertEquals(facadPut.price(amerBinom), 3.188768817646037, .00001);
     }
 }
